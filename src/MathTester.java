596ccdc1ee2a3975e49dc8956dcfd7275762beb3
eyuel <eyuel.negash.2022@student.lhschools.org>
2019-02-14 13:03:15 -0500

    printed variables

--- src/MathTester.java
+++ src/MathTester.java
@@ -12,6 +12,17 @@ public class MathTester {
         secondnumber= 10;
         total=firstnumber+secondnumber;
 
+        System.out.println(total);
+
+        System.out.println(firstWord+secondWord);
+        System.out.println(firstWord+(firstnumber+secondnumber));
+        System.out.println("firstnumber+secondnumber is "+(firstnumber+secondnumber));
+        System.out.println("firstnumber-secondnumber is "+(firstnumber-secondnumber));
+        System.out.println("secondnumber / firstnumber is"+(secondnumber/firstnumber));
+        System.out.println("firstnumber*secondnumber is "+(firstnumber*secondnumber));
+        System.out.println("The modulus of secondnumber by the firstnumber is "+ (secondnumber%firstnumber));
+        System.out.println(total);
+
 
 
 
